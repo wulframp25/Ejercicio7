1d0de640a5d80a650f58bcda0b764a964b79a203
wulframp25 <wulfram.polo@udea.edu,co>
2021-05-01 21:50:34 -0500

    Se actualiza el funcionamiento de la time.c

--- time.c
+++ time.c
@@ -9,9 +9,9 @@
 struct timeval starting_time, ending_time;
 
 int main() {
-char str[100];
-char *delim;
-char buf[100];
+    char str[100];
+    char *delim;
+    char buf[100];
 
     printf("time <command> ");
 	fgets(str, 100, stdin);
@@ -27,31 +27,23 @@ char buf[100];
     pid_t pid; 
 
     pid =fork(); 
+    gettimeofday(&starting_time, NULL);
     
     if (pid==-1){ 
     printf("Error al crear el p Hijo");
     exit(0); 
     }
-    if (pid) { //Proceso padre 
-       // printf("Soy P, PID Hijo %d\n", pid);
-       gettimeofday(&starting_time, NULL);
+    if (pid) { //Proceso padre        
+        wait(NULL);
         gettimeofday(&ending_time, NULL);
         int timeH = ((ending_time.tv_sec * 1000000 + ending_time.tv_usec)-(starting_time.tv_sec *  1000000 + starting_time.tv_usec));
         printf("tiempo : %d micro seconds\n", timeH);
-        } else { //Proceso hijo
-            wait(NULL); 
-            printf("Voy a ejecutar pero ");
-            status=execv(args[0], args);
-            if(status<0){
+        } else { //Proceso hijo       
+            printf("Voy a eje utar pero ");
+            status=execv(args[0], args);            
+        if(status<0){
             printf("Error! %s no se reconoce o no se pudo ejecutar", str);
-            exit(0); 
-        gettimeofday(&starting_time, NULL);
-        gettimeofday(&ending_time, NULL);
-        int timeH = ((ending_time.tv_sec * 1000000 + ending_time.tv_usec)-(starting_time.tv_sec * 1000000 + starting_time.tv_usec));
-        printf("Tiempo : %d micro seconds\n", timeH);
-
-
+            exit(0);     
         }
-
     }
 }
